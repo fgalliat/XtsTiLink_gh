fd89bb7ce3ac168ea198af5c3f1d646f61808171
Franck GALLIAT <Franck GALLIAT>
2017-06-19 13:06:15 +0200

    UPDATED scripts

--- tools/sendTipi.sh
+++ tools/sendTipi.sh
@@ -3,3 +3,6 @@ export rpi="192.168.1.16"
 
 scp /vm_mnt/devl/TI_92/Arduino/XtsTiLink/devl4ti/test_gcc4ti_1/xtsterm.92p root@$rpi:/vm_mnt/XtsTiLink/devl4ti/test_gcc4ti_1/
 scp /vm_mnt/devl/TI_92/Arduino/XtsTiLink/tools/welcm.sh root@$rpi:/vm_mnt/XtsTiLink/tools/
+
+scp /vm_mnt/devl/TI_92/Arduino/XtsTiLink/tools/src/SyncArduinoPipe.java root@$rpi:/vm_mnt/XtsTiLink/tools/src/
+scp /vm_mnt/devl/TI_92/Arduino/XtsTiLink/tools/src/Shell.java root@$rpi:/vm_mnt/XtsTiLink/tools/src/
